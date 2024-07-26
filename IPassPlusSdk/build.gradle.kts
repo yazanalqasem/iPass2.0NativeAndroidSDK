fa5faf2fa35690291247a8817f7d2619cbd7d843
MobileDev1 <yalqasem@ipass-mena.com>
2024-07-26 18:21:52 +0530

    language support

--- IPassPlusSdk/build.gradle.kts
+++ IPassPlusSdk/build.gradle.kts
@@ -100,7 +100,7 @@ project.afterEvaluate {
                 from(components["release"])
                 groupId = "com.sdk.ipassplussdk"
                 artifactId = "iPass2.0NativeAndroidSDK"
-                version = "1.0.6"
+                version = "1.0.7"
             }
         }
     }
