956974484e5da32e29e74f4add95389d2e2c75c8
MobileDev1 <yalqasem@ipass-mena.com>
2024-08-09 23:36:04 +0530

    custom db basic

--- IPassPlusSdk/build.gradle.kts
+++ IPassPlusSdk/build.gradle.kts
@@ -101,7 +101,7 @@ project.afterEvaluate {
                 from(components["release"])
                 groupId = "com.sdk.ipassplussdk"
                 artifactId = "iPass2.0NativeAndroidSDK"
-                version = "1.0.25"
+                version = "1.0.26"
             }
         }
     }
