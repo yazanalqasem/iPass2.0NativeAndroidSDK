e239218392d5196afb55aa075f4458cdf67c97cd
MobileDev1 <yalqasem@ipass-mena.com>
2024-08-09 13:04:54 +0530

    full auth db

--- IPassPlusSdk/build.gradle.kts
+++ IPassPlusSdk/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("com.regula.documentreader:api:7.3.9948+@aar") {
         this.isTransitive = true
     }
-    implementation("com.regula.documentreader.core:fullauthrfid:7.3.11255@aar")
+//    implementation("com.regula.documentreader.core:fullauthrfid:7.3.11255@aar")
 //    implementation("com.github.yazanalqasem:iPass2.0CoreAndroidSDK:2.1")
 
     // FaceLivenessDetector dependency
@@ -101,7 +101,7 @@ project.afterEvaluate {
                 from(components["release"])
                 groupId = "com.sdk.ipassplussdk"
                 artifactId = "iPass2.0NativeAndroidSDK"
-                version = "1.0.10"
+                version = "1.0.11"
             }
         }
     }
