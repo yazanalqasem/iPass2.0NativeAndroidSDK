333855f339cf7c24912c084d6ce5bb172fa8aafc
MobileDev1 <yalqasem@ipass-mena.com>
2024-08-09 21:34:24 +0530

    basic database 1

--- IPassPlusSdk/src/main/java/com/sdk/ipassplussdk/core/IPassSDK.kt
+++ IPassPlusSdk/src/main/java/com/sdk/ipassplussdk/core/IPassSDK.kt
@@ -455,7 +455,7 @@ object iPassSDKManger {
                         ip.toString(),
                         flowId,
 //                        "androidSdk",
-                        "Android v1.0.19(CustomDbBasic)",
+                        "Android v1.0.20(CustomDbBasic)",
                         "0",
                         callback
                     )
@@ -464,7 +464,7 @@ object iPassSDKManger {
                         context, email, userToken, appToken, socialMediaEmail, phoneNumber,
                         ip.toString(),
 //                        flowId, "androidSdk", bindingView, callback
-                        flowId, "Android v1.0.19(CustomDbBasic)", bindingView, callback
+                        flowId, "Android v1.0.20(CustomDbBasic)", bindingView, callback
                     )
                 }
             } catch (e: Exception ) {
